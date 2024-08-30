2cdf7c63fc7a8032a8682d549af5c73f23aae83e
zukapa <kmvsec@gmail.com>
2024-08-30 14:47:33 +0300

    Lesson seventh complete

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
-URN = '/catalogData.json'
+URL_PRODUCTS = ''
+URN_PRODUCTS = '/catalog'
+URL_BASKET = ''
+URN_BASKET = '/basket'
+URL_ADD_TO_BASKET = ''
+URN_ADD_TO_BASKET = '/addToBasket'
+URL_DELETE_FROM_BASKET = ''
+URN_DELETE_FROM_BASKET = '/deleteFromBasket'
 
 const { createApp } = Vue
 
@@ -26,6 +32,20 @@ const app = createApp({
             });
         },
 
+        postDataAPI(url, data) {
+            return new Promise((resolve, reject) => {
+                let xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        resolve(xhr.responseText);
+                    }
+                }
+                xhr.open('POST', url, true);
+                xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+                xhr.send(JSON.stringify(data));
+            });
+        },
+
         calculationTotalCost() {
             return this.products.reduce((accumulator, product) => Object.keys(product).length === 0 ? accumulator : accumulator += product.price, 0);
         },
@@ -46,7 +66,7 @@ const app = createApp({
 
     mounted() {
         setTimeout(() => {
-            this.getDataAPI(`${URL}${URN}`).then(products => {
+            this.getDataAPI(`${URL_PRODUCTS}${URN_PRODUCTS}`).then(products => {
                 this.products = JSON.parse(products);
                 this.filteredProducts = this.products;
             });
@@ -55,42 +75,117 @@ const app = createApp({
 })
 
 app.component('product-component', {
-    props: ['good'],
+    props: ['good',
+            'postIntoBasketProduct'],
+
+    data() {
+        return {
+            product: {}
+        }
+    },
+
+    methods: {
+        postIntoBasket() {
+            this.postIntoBasketProduct(`${URL_ADD_TO_BASKET}${URN_ADD_TO_BASKET}`, this.product).then(result => {
+                console.log(result);
+            });
+        }
+    },
+
+    mounted() {
+        this.product = {
+            "id_product": this.good.id_product,
+        }
+    },
+
     template: `
-        <div class="product-item">
+        <div>
             <img class=product-image></img>
             <h4>{{ good.product_name }}</h4>
             <p>{{ good.price }}</p>
+            <button-shop @button-action="postIntoBasket" class="product-into-basket-button">Into a basket</button-shop>
         </div>`
 })
 
 app.component('products-component', {
-    props: ['products'],
+    props: ['products',
+            'postIntoBasket'],
+
     template: `
-        <product-component class="products-item" v-for="product in products" :good="product"></product-component>`
+        <product-component class="products-item" v-for="product in products" :good="product" :post-into-basket-product="postIntoBasket"></product-component>`
 })
 
 app.component('search-component', {
     props: ['modelValue'],
+
     emits: ['update:modelValue'],
+
     template: `
         <div class="search-line">
             <input class="search-line-item" type="text" placeholder="Search" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
         </div>`
 })
 
-app.component('basket-component', {
-    props: ['visibleBasket'],
+app.component('basket-products-component', {
+    props: ['good',
+            'deleteProductFromBasket'],
+
+    data() {
+        return {
+            product: {}
+        }
+    },
+
+    methods: {
+        deleteFromBasket() {
+            this.deleteProductFromBasket(`${URL_DELETE_FROM_BASKET}${URN_DELETE_FROM_BASKET}`, this.product).then(result => {
+                console.log(result);
+            });
+        }
+    },
+
+    mounted() {
+        this.product = {
+            "id_product": this.good.id_product,
+        }
+    },
+
     template: `
-        <div v-if="visibleBasket" class="basket">
-            <p class="header-basket">Basket</p>
-            <button @click="$emit('change-visible-basket')" class="basket-close-button">Close</button>
+        <div>
+            <p class="basket-product-info">Product: {{ good.product_name }};</p>
+            <p class="basket-product-info">count: {{ good.count }}</p>
+            <button-shop @button-action="deleteFromBasket" class="delete-from-basket-button">Delete</button-shop>
         </div>`
 })
 
-app.component('button-basket-component', {
+app.component('basket-component', {
+    props: ['changeVisibleBasket', 
+            'getBasket',
+            'deleteFromBasket'],
+
+    data() {
+        return {
+            basket: []
+        }
+    },
+
+    mounted() {
+        this.getBasket(`${URL_BASKET}${URN_BASKET}`).then(basket => {
+            this.basket = JSON.parse(basket);
+        });
+    },
+
     template: `
-        <button @click="$emit('change-visible-basket')" class="basket-button menu-button" type="button"><slot /></button>`
+        <div class="basket">
+            <p class="header-basket">Basket</p>
+            <basket-products-component class="basket-products-item" v-for="product in this.basket" :good="product" :delete-product-from-basket="deleteFromBasket"></basket-products-component>
+            <button-shop @button-action="changeVisibleBasket" class="basket-close-button">Close</button-shop>
+        </div>`
+})
+
+app.component('button-shop', {
+    template: `
+        <button @click="$emit('button-action')" type="button"><slot /></button>`
 })
 
 app.mount('#app')
---
+++ server/server.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const fs = require('fs');
+const bodyparser = require('body-parser');
+
+const app = express();
+
+app.use(express.static('..'));
+app.use(bodyparser.json());
+
+app.get('/catalog', (req, res) => {
+    fs.readFile('./static/catalog-products.json', 'utf8', (err, data) => {
+        res.send(data);
+    });
+});
+
+app.get('/basket', (req, res) => {
+    fs.readFile('./static/basket-buyer.json', 'utf8', (errBasket, dataBasket) => {
+        fs.readFile('./static/catalog-products.json', 'utf8', (errProducts, dataProducts) => {
+            const basket = JSON.parse(dataBasket);
+            const products = JSON.parse(dataProducts);
+
+            const basketCombined = basket.map(product => {
+                const productInCatalog = products.find(element => element.id_product === product.id_product);
+                return {
+                    ...product,
+                    ...productInCatalog
+                }
+            })
+
+            res.send(basketCombined);
+        })
+    });
+});
+
+app.post('/addToBasket', (req, res) => {
+    fs.readFile('./static/basket-buyer.json', 'utf8', (err, data) => {
+        if (err) {
+            res.send('{"result": 0}');
+        } else {
+            const basket = JSON.parse(data);
+            const product = req.body;
+            product.count = 1;
+
+            const productInBasket = basket.find(element => element.id_product === product.id_product);
+            
+            if (productInBasket) {
+                productInBasket.count += 1;
+            } else {
+                basket.push(product);
+            }
+
+            fs.writeFile('./static/basket-buyer.json', JSON.stringify(basket), 'utf8', (err) => {
+                if (err) {
+                    res.send('{"result": 0}');
+                } else {
+                    res.send('{"result": 1}');
+                }
+            });
+        }
+    });
+});
+
+app.post('/deleteFromBasket', (req, res) => {
+    fs.readFile('./static/basket-buyer.json', 'utf8', (err, data) => {
+        if (err) {
+            res.send('{"result": 0}');
+        } else {
+            let basket = JSON.parse(data);
+            const product = req.body;
+
+            const productInBasket = basket.find(element => element.id_product === product.id_product);
+
+            if (productInBasket) {
+                if (productInBasket.count === 1) {
+                    basket = basket.filter(product => {return product.id_product != productInBasket.id_product})
+                } else {
+                    productInBasket.count -= 1;
+                }
+            } else {
+                console.log('The product does not exist in the shopping basket');
+            }
+
+            if (basket.length === 0) {
+                basket = [];
+            }
+
+            fs.writeFile('./static/basket-buyer.json', JSON.stringify(basket), 'utf8', (err) => {
+                if (err) {
+                    res.send('{"result": 0}');
+                } else {
+                    res.send('{"result": 1}');
+                }
+            });
+        }
+    });
+});
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
